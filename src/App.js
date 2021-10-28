195b84d46f692297a0ce956a5d6aa7bec4f2c0d9
afeef <afeef@sanoft.com>
2021-10-28 16:09:15 +0530

    fix: design issues are fixed * design updated * todo delete function updated * button disabled feature updated
    Updated By: Afeef

--- src/App.js
+++ src/App.js
@@ -1,39 +1,64 @@
 import "./App.css";
 import { useState } from "react";
+import styles from "./styles.module.css";
 function App() {
   // state for todoList
   const [todoList, setTodoList] = useState();
+
   // state for todo
   const [todo, setTodo] = useState();
+
   // handle todo onChange state
   const handleTodoChange = (event) => setTodo(event.target.value);
+
   // handle clear the state
   const clearCaches = () => setTodo("");
+
   // handle add todo to list
   const handleAddTodo = () => {
     setTodoList(todoList ? [...todoList, todo] : [todo]);
-    clearCaches();
+    clearCaches(); //clear the todo input
   };
+
   // handle delete action in todo
   const handleDltBtn = (name) =>
     setTodoList(todoList.filter((todo) => todo !== name));
   return (
-    <div className="body">
-      <div>
-        <div className="hed">To-do-App!</div>
-        <div className="newto">Add New To-Do</div>
-          <div>
-            <div>{todoList && todoList.map((todo) => <p>{todo}</p>)}</div>
-              <div>
-               <input className="box"
-                type="text"
-                name="todoName"
-                value={todo}
-                onChange={handleTodoChange}
-               />
-               <button className="add" onClick={handleAddTodo}>ADD</button>
-              </div>
+    <div className={styles.contentWrapper}>
+      <div className={styles.container}>
+        <div className={styles.appTitle}>To-do-App!</div>
+        <div className={styles.subTitle}>Add New To-Do</div>
+        <div className={styles.todoSection}>
+          <div className={styles.todoList}>
+            {todoList &&
+              todoList.map((todo) => (
+                <div className={styles.todoListItem}>
+                  <p className={styles.todoName}>{todo}</p>
+                  <button
+                    className={styles.todoDltBtn}
+                    onClick={() => handleDltBtn(todo)}
+                  >
+                    <i class="fas fa-trash"></i>
+                  </button>
+                </div>
+              ))}
           </div>
+          <div className={styles.todoInputs}>
+            <input
+              className={styles.todoInput}
+              type="text"
+              name="todoName"
+              value={todo}
+              onChange={handleTodoChange}
+            />
+            <button
+              className={todo ? styles.addBtn : styles.disabledAddBtn}
+              onClick={todo ? handleAddTodo : null}
+            >
+              ADD
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
